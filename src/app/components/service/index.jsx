c3bb231e702a2d345b7322eac934bdac6b76c9a5
osuji chiaka <chiakajunior@yahoo.com>
2025-03-01 00:58:36 +0300

    checking

--- src/app/components/service/index.jsx
+++ src/app/components/service/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Service() {
     return (
-      <section className="pt-[15rem] text-[1.6rem] ourService_section"> 
+      <section className="pt-[10rem] text-[1.6rem] ourService_section"> 
       <div className="bg-primary rounded-[5px] text-[#fff] font-[500] w-[15rem] h-[45px] px-[2rem] mx-auto flex items-center justify-center">OUR SERVICES</div>
        
       </section>
